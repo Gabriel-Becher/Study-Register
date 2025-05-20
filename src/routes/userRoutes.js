a735a99b75208e2af7bc2d6d9bf77d2558eff49d
Gabriel A. Becher <gabriel.abecher@hotmail.com>
2025-05-20 12:55:28 -0300

    prepared rep to start frontend dev

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const userController = require("../controllers/userController");
-const tokenController = require("../controllers/tokenController");
 const router = Router();
 
 router.get("/", userController.getAllUsers);
-router.get("/:id", tokenController.verifyToken, userController.getUserById);
+router.get("/:id", userController.getUserById);
 router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
